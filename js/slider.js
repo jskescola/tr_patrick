335785365ef52346fd6d2b7394f876532b3eef50
jskescola <86962206+jskescola@users.noreply.github.com>
2021-07-05 09:14:02 -0300

    Subindo os arquivos atualizados do projeto

---
+++ js/slider.js
@@ -0,0 +1,34 @@
+$('.livrosDestaques').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1400,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
